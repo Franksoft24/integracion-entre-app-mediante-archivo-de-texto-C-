d0072b4122a2f23133e1c4e3634ac041c8062d06
Franksoft24 <frankmer95@gmail.com>
2015-05-22 14:45:36 -0400

    Creacion del repositorio git

---
+++ Escribir archivo de texto/Escribir archivo de texto/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Escribir_archivo_de_texto
+{
+    class Program
+    {
+        public SqlConnection con = new SqlConnection();
+        static void Main(string[] args)
+        {
+            /***************************************************************************
+             
+             *  Para que esta app pueda funcionar devera de:
+             *  
+             *      Crear la db con una estructura => 
+             *          -Nombre: Empresa, 
+             *          -Tabla: Nomina, 
+             *          -Atributos: Cedula, Nombre, Cuenta y Monto.
+             *          
+             *              _____________
+             *              |  NOMINA   |
+             *              |-----------|
+             *              |  Cedula   |
+             *              |  Nombre   |
+             *              |  Cuenta   |
+             *              |  Monto    |
+             *              -------------
+             * 
+             *      Cambiar el driver de coneccion (password, userID, Ubicacion).
+             *      De ser necesario la ubicacion de donde se esta guardando el archivo de texto.
+             *      
+             *  Funcionalidad o proposito de la app:
+             *  
+             *      Leer una Base de datos.
+             *      Generar archivo de texto con los datos leido.
+             *      
+             *  Comentario:
+             *      ...
+             *      
+             ***************************************************************************/
+            //Conectamo a la db
+            SqlConnection con = new SqlConnection("Data Source=FRANKSOFT;Initial Catalog=Empresa;Integrated Security=True");
+            con.Open();//Abrimos conexion
+            string textoCmd = "Select * from Nomina";//Creamos la consulta a DB
+            SqlCommand cmd = new SqlCommand(textoCmd, con);//Ejecutamos la consulta
+            SqlDataReader leer = cmd.ExecuteReader();//Leemos los valores que retornan
+            string[] lines = new string[1000];//Creamos array para recivir los valores
+            string Cedula;
+            string Nombre;
+            string Cuenta;
+            string Monto;
+            string concatenar;
+            int conter = 0;
+            while (leer.Read())
+            {//Pasamos los datos obtenidos a el array
+                Cedula = String.Format("{0}", leer[0]);
+                Nombre = String.Format("{0}", leer[1]);
+                Cuenta = String.Format("{0}", leer[2]);
+                Monto = String.Format("{0}", leer[3]);
+                concatenar = Cedula + "," + Nombre + "," + Cuenta + "," + Monto;
+                lines[conter] = concatenar;
+                conter++;
+
+            }
+            System.IO.File.WriteAllLines(@"D:\Nomina.txt", lines);//Generamos y guardamos archivo de texto.
+            con.Close();//Cerramos conexion a db.
+            Console.Write("Wiiiiii. Se ha generado el archivo de texto :p. \nOshe que rico!!! :V");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Leer archivo de texto/Leer archivo de texto/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Leer_archivo_de_texto
+{
+    class Program
+    {
+        public SqlConnection con = new SqlConnection();
+        static void Main(string[] args)
+        {
+
+            /***************************************************************************
+             
+             *  Para que esta app pueda funcionar devera de:
+             *  
+             *      Crear la db con una estructura => 
+             *          -Nombre: Banco, 
+             *          -Tabla: Nomina, 
+             *          -Atributos: Cedula, Nombre, Cuenta y Monto.
+             *          
+             *              _____________
+             *              |  NOMINA   |
+             *              |-----------|
+             *              |  Cedula   |
+             *              |  Nombre   |
+             *              |  Cuenta   |
+             *              |  Monto    |
+             *              -------------
+             * 
+             *      Cambiar el driver de coneccion (password, userID, Ubicacion).
+             *      De ser necesario la ubicacion de donde se esta guardando el archivo de texto.
+             *      
+             *  Funcionalidad o proposito de la app:
+             *  
+             *      Leer un archivo de texto ya generado por otra aplicacion.
+             *      Insertar campos en la base de datos.
+             *      
+             *  Comentario:
+             *      ...
+             *      
+             ***************************************************************************/
+
+
+            //Conectamo a la db
+            SqlConnection con = new SqlConnection("Data Source=FRANKSOFT;Initial Catalog=Banco;Integrated Security=True");
+            string[] Empleados = System.IO.File.ReadAllLines(@"D:\Nomina.txt");//Bucamos la direccion del archivo
+            string[] InsEmpleado;//obtendra los datos del archivo.
+            foreach (string empleado in Empleados)
+            {//Recorremos todas las lineas del archivo
+                InsEmpleado = empleado.Split(new Char[] {','});//Separamos los datos en un array
+                if (!String.IsNullOrEmpty(InsEmpleado[0]))
+                {//verificamos que la cadena que este pasando no este vacia 
+                    con.Open();//Abrimos conexion
+                    //Creamos el script
+                    string textoCmd = "INSERT INTO Nomina VALUES ('" + InsEmpleado[0] + "','" + InsEmpleado[1] + "','" + InsEmpleado[2] + "','" + InsEmpleado[3] + "')";//Creamos la consulta a DB
+                    SqlCommand cmd = new SqlCommand(textoCmd, con);//Ejecutamos la consulta
+                    cmd.ExecuteReader();
+                    con.Close();//Cerramos conexion 
+                }
+            }
+            Console.Write("\nEsto Fue todo amigo, hehehe hehehe hehehehhehehe.");
+            System.Console.ReadKey();
+        }
+    }
+}
